a2bbf50021f4ee9c23f352991e3c2221039d84af
CollinsTatang <makungongcollins@gmail.com>
2021-06-16 15:19:10 +0100

    Add category model and update the other table related to it

---
+++ db/migrate/20210616121023_create_articles.rb
@@ -0,0 +1,14 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :text
+      t.string :image
+      t.date :createAt
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
