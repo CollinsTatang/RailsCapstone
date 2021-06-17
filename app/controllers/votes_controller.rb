6dc4dd837cfd4ed81c5476e65a4206a2bddb0406
CollinsTatang <makungongcollins@gmail.com>
2021-06-17 10:03:39 +0100

    Fix rubocop error

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,27 +1,26 @@
 class VotesController < ApplicationController
-    before_action :require_login
-    def create
-      @vote = current_user.votes.new(article_id: params[:article_id])
-      @article = Article.find_by(id: @vote.article.id)
-      if @vote.save
-        redirect_to category_path(@article.category)
-        flash[:notice] = 'You Voted this article'
-      else
-        flash[:notice] = 'Somthing happened'
-      end
+  before_action :require_login
+  def create
+    @vote = current_user.votes.new(article_id: params[:article_id])
+    @article = Article.find_by(id: @vote.article.id)
+    if @vote.save
+      redirect_to category_path(@article.category)
+      flash[:notice] = 'You Voted this article'
+    else
+      flash[:notice] = 'Somthing happened'
     end
-  
-    def destroy
-      vote = Vote.find_by(user: current_user, article_id: params[:article_id])
-      @article = Article.find_by(id: vote.article.id)
-      if vote
-        vote.destroy
-        redirect_to category_path(@article.category)
-        flash[:notice] = 'You Unvoted this article'
-      else
-        redirect_to root_path
-        flash[:notice] = 'somthing happened'
-      end
+  end
+
+  def destroy
+    vote = Vote.find_by(user: current_user, article_id: params[:article_id])
+    @article = Article.find_by(id: vote.article.id)
+    if vote
+      vote.destroy
+      redirect_to category_path(@article.category)
+      flash[:notice] = 'You Unvoted this article'
+    else
+      redirect_to root_path
+      flash[:notice] = 'somthing happened'
     end
   end
 end
