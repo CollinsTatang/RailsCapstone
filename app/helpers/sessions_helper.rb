9b6e3988584219e937144bf6af8cc805476e1833
CollinsTatang <makungongcollins@gmail.com>
2021-06-21 03:56:45 +0100

    Add user_sessions helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+    def user_sessions
+        if current_user
+            (link_to "Home", root_path, class: 'nav-link') + 
+            (link_to "Create Article", new_article_path, class: 'nav-link') + 
+            (link_to "Create Category", new_category_path, class: 'nav-link') + 
+            (link_to 'Logout', logout_path, class: 'nav-link')     
+        else   
+            (link_to "Home", root_path, class: 'nav-link') + 
+            (link_to 'Login', new_session_path, class: 'nav-link') + 
+            (link_to 'Sign up', new_user_path, class: 'nav-link')    
+        end 
+    end
 end
